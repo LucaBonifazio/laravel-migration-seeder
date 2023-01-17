7055d0c125863c0d8bfc4f51dd798d465ee1f8db
LucaBonifazio <luca.bonifazio13@gmail.com>
2023-01-17 13:42:39 +0100

    first try for migrate

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Trains;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+
     public function main() {
-        return view('main');
+        $table = Trains::all();
+        return view('main', [
+            'table' => $table,
+        ]);
     }
 }
---
+++ database/migrations/2023_01_17_122847_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 100);
+            $table->string('departure_station', 250);
+            $table->string('arrival_station', 250);
+            $table->date('departure_time');
+            $table->date('arrival_time');
+            $table->string('code', 50);
+            $table->smallInteger('n_carriages');
+            $table->date('on_time');
+            $table->date('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
