bcaa9b5f12d71fcf0a606af79e08de57599cfb94
LucaBonifazio <luca.bonifazio13@gmail.com>
2023-01-17 16:33:45 +0100

    print on page but don't use seeder

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
     <div>
-        {{$table}}
+        {{$table[0]['agency']}}
+    </div>
+    <div>
+        {{$table[0]['departure_station']}}
+    </div>
+    <div>
+        {{$table[0]['arrival_station']}}
+    </div>
+    <div>
+        {{$table[0]['departure_time']}}
     </div>
 @endsection
