ffc87c935b8dcc46977d243585ff3a443d123162
LucaBonifazio <luca.bonifazio13@gmail.com>
2023-01-18 13:22:01 +0100

    need fix style

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,18 +1,23 @@
 @extends('guest.layouts.base')
 
 @section('content')
+<div class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-2 p-5">
     @foreach ($trains as $train)
-    <div>
-        {{$train['company']}} <br>
-        {{$train['departure_station']}} <br>
-        {{$train['arrival_station']}} <br>
-        {{$train['departure_time']}} <br>
-        {{$train['arrival_time']}} <br>
-        {{$train['code']}} <br>
-        {{$train['n_carriages']}} <br>
-        {{$train['on_time']}} <br>
-        {{$train['deleted']}}
+    <div class="col">
+        <div class="card h-100">
+            <div class="card-body">
+                <div class="card-text">{{$train['company']}}</div>
+                <div class="card-text">{{$train['departure_station']}}</div>
+                <div class="card-text">{{$train['arrival_station']}}</div>
+                <div class="card-text">{{$train['departure_time']}}</div>
+                <div class="card-text">{{$train['arrival_time']}}</div>
+                <div class="card-text">{{$train['code']}}</div>
+                <div class="card-text">{{$train['n_carriages']}}</div>
+                <div class="card-text">{{$train['on_time']}}</div>
+                <div class="card-text">{{$train['deleted']}}</div>
+            </div>
+        </div>
     </div>
-    <br>
     @endforeach
+</div>
 @endsection
